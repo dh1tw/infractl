0799c4427dc49f8ba55a9a72d10ae1701b9c0147
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2019-07-06 23:22:45 +0200

    make sure route names are always lower case

--- microtik/options.go
+++ microtik/options.go
@@ -1,5 +1,7 @@
 package microtik
 
+import "strings"
+
 // RouteID is a functional option. It is used when working with ip/routes
 // on microtik router. In order to work with routes there needs to be a
 // way to identify them. This most reliable way is to identify them through
@@ -8,6 +10,7 @@ package microtik
 // router.
 func RouteID(name, comment string) Option {
 	return func(m *Microtik) {
+		name = strings.ToLower(name)
 		m.routeIDs[name] = comment
 	}
 }
